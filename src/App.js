952c9bd5d35627ade5da3c56a46287e3f0b378df
Sebastian camilo cruz dussan <sebastiankmilo1996@gmail.com>
2020-07-09 11:16:41 -0500

    Se implementaron en la pagina principales los componentes que se usaron ara explicar los pasos anteriores

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { Component } from "react";
+import React  from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import RenderizadoCondicional from "./Renderizado_condicional";
 import Trabajarconlistas from "./TrabajarconListas"
 import PropsyEstate from "./PropsyEstate"
+import Eventos from "./Eventos"
+import Formulario from "./Formularios"
+import PropsChildren from "./PropsChildrenyPrototype"
 
 
 
@@ -11,7 +14,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <PropsChildren
+          author="Sebastian"
+          date={new Date().toLocaleDateString()}
+          title="Articulo sobre la Propiedad Children">
+          <p>Esto es un articulo que estoy pasando sus elementos como hijos de un componente en react :D</p>
+          <strong>Y Mantiene las etiquetas y componentes que hayas añaido dentro del componente</strong>
+        </PropsChildren>
         <img src={logo} className="App-logo" alt="logo" />
+        <Formulario />
+        <Eventos />
         <Trabajarconlistas numeros={[1,2,3,45]} />
         <RenderizadoCondicional />
         <PropsyEstate />
